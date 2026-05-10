99897aad2d125b21796c9f303c77e167719234df
icezingza <icezingza@gmail.com>
2026-05-11 02:51:23 +0700

    Add research agent foundation

---
+++ agents/research_agent.py
@@ -0,0 +1,11 @@
+class ResearchAgent:
+    """Autonomous scientific research agent."""
+
+    def analyze_papers(self, papers):
+        return []
+
+    def generate_hypotheses(self, domain):
+        return []
+
+    def detect_research_gaps(self):
+        return []
