0f2ef4e72b5808ee05e609828e9e80a6c509866f
icezingza <icezingza@gmail.com>
2026-05-15 13:53:55 +0700

    Add recursive reflection engine

---
+++ src/reasoning/reflection_engine.py
@@ -0,0 +1,8 @@
+class ReflectionEngine:
+    """Recursive self-reflection engine."""
+
+    def reflect(self, reasoning):
+        return {
+            'original': reasoning,
+            'reflection': 'Reasoning reviewed'
+        }
